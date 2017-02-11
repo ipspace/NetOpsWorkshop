9fa3cb923cd2ab83f95cf4d3298868d31da82098
Ivan Pepelnjak <ip@ipspace.net>
2017-02-11 17:32:38 +0100

    Make software installation quieter

--- install/install.sh
+++ install/install.sh
@@ -3,24 +3,25 @@
 # Update to latest release
 #
 echo "Update installed software to latest release"
-sudo apt-get update
+sudo apt-get -qq update
 #
 # Install missing packages
 #
 echo "Install missing packages"
-sudo apt-get install python-setuptools python-pip git ack-grep jq
+sudo apt-get -qq install python-setuptools python-pip git ack-grep jq
 #
 # Install Python components
 #
 echo "Install required Python components"
-sudo pip install PyYAML jinja2 httplib2 six bracket-expansion pysnmp netaddr
+sudo apt-get -qq install python-yaml python-httplib2 python-pysnmp4
+sudo pip install jinja2 six bracket-expansion netaddr
 #
 echo "Install optional Python components"
 sudo pip install yamllint
 #
 # Install latest stable Ansible version from Ansible reposityr
 #
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get -qq install software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get -qq update
+sudo apt-get -qq install ansible
