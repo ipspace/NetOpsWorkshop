216b9eadf94284646207758ac833991223032ae4
Ivan Pepelnjak <ip@ipspace.net>
2017-11-05 19:09:00 +0100

    New options for the render script

--- Jinja2/render.py
+++ Jinja2/render.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+import getopt
 import re
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Undefined, StrictUndefined, make_logging_undefined
 import yaml
 from bracket_expansion import *
 from pprint import pprint
@@ -10,22 +11,60 @@ from pprint import pprint
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
-trim = True
-if (len(sys.argv) > 2):
-  trim = sys.argv[2] != 'notrim'
+def getOptions():
+  try:
+    options, args = getopt.getopt(sys.argv[1:], "y:j:n:sw", ["yaml=", "jinja=", "notrim", "strict", "warning"])
+  except getopt.GetoptError as err:
+    # print help information and exit:
+    print str(err)  # will print something like "option -a not recognized"
+    sys.exit(2)
 
-ENV = Environment(loader=FileSystemLoader('.'),trim_blocks=trim,lstrip_blocks=trim)
+  global yamlfile,jinjafile,trim,undefined
+  trim = True
+  opts = 0
+
+  for opt,arg in options:
+    opts = opts + 1
+    if opt in ("-y","-yaml"):
+      yamlfile = arg
+    elif opt in ("-j","-jinja"):
+      jinjafile = arg
+    elif opt in ("-n","-notrim"):
+      trim = False
+    elif opt in ("-w","-warning"):
+      undefined = make_logging_undefined (base = Undefined)
+    elif opt in ("-s","-strict"):
+      undefined = make_logging_undefined (base = StrictUndefined)
+
+  return opts > 0
+
+trim = True
+undefined = Undefined
+yamlfile = None
+jinjafile = None
+
+if not getOptions():
+  if (len(sys.argv) > 2):
+    trim = sys.argv[2] != 'notrim'
+
+ENV = Environment(loader=FileSystemLoader('.'),trim_blocks=trim,lstrip_blocks=trim,undefined=undefined)
 ENV.filters['bracket_expansion'] = bracket_expansion
 
 filename = re.sub("\.$","",sys.argv[1])
 
-print ('--- Reading YAML file '+filename+'.yml ---')
-with open(filename + '.yml') as _: yamldict = yaml.load(_)
+if yamlfile is None:
+  yamlfile = filename+".yml"
 
-print ('--- YAML dictionary in '+filename+'.yml ---')
+if jinjafile is None:
+  jinjafile = filename+".j2"
+
+print ('--- Reading YAML file '+yamlfile+' ---')
+with open(yamlfile) as _: yamldict = yaml.load(_)
+
+print ('--- YAML dictionary in '+yamlfile+' ---')
 pprint(yamldict)
 print
 
-print ('--- Rendering template '+filename+'.j2 ---') 
-template = ENV.get_template(filename + ".j2")
+print ('--- Rendering template '+jinjafile+' ---') 
+template = ENV.get_template(jinjafile)
 print(template.render(**yamldict))
